2cef247569f832fce6d0c86f0b96295d52a2d040
Damien Tan <dtjldamien@gmail.com>
2020-01-22 01:30:43 +0800

    q5

---
+++ Assignment1/A0183560E-5.py
@@ -0,0 +1,15 @@
+import csv
+import math
+
+with open('resale-flat-prices.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    with open('q5-ouput.csv', 'w', newline='') as output:
+        csv_writer = csv.writer(output)
+
+        for line in csv_reader:
+            if line[1] == 'YISHUN':
+                floorArea = float(line[6])
+                floorArea = math.ceil(1.1 * floorArea) 
+                line[6] = str(floorArea)
+            csv_writer.writerow(line)
